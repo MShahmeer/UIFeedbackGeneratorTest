710938abe38b315111cb8aeffa9eb728721b3e5e
MShahmeer <shahmeer.athar@gmail>
2017-07-11 10:21:29 +0500

    Completed

--- UIFeedbackGeneratorTest/ViewController.swift
+++ UIFeedbackGeneratorTest/ViewController.swift
@@ -9,10 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var impactFeedbackGenerator: UIImpactFeedbackGenerator? = nil
+    var selectionFeedbackGenerator: UISelectionFeedbackGenerator? = nil
+    var notificationFeedbackGenerator: UINotificationFeedbackGenerator? = nil
+    
+    @IBAction func selectionChangedButtonPressed(_ sender: Any) {
+        selectionFeedbackGenerator?.selectionChanged()
+    }
+    
+    @IBAction func notificationSuccessButtonPressed(_ sender: Any) {
+        notificationFeedbackGenerator?.notificationOccurred(.success)
+    }
+    
+    @IBAction func notificationWarningButtonPressed(_ sender: Any) {
+        notificationFeedbackGenerator?.notificationOccurred(.warning)
+    }
+    
+    @IBAction func notificationErrorButtonPressed(_ sender: Any) {
+        notificationFeedbackGenerator?.notificationOccurred(.error)
+    }
+    
+    @IBAction func impactLightButtonPressed(_ sender: Any) {
+        impactFeedbackGenerator = UIImpactFeedbackGenerator(style: .light)
+        impactFeedbackGenerator?.impactOccurred()
+    }
+    
+    @IBAction func impactMediumButtonPressed(_ sender: Any) {
+        impactFeedbackGenerator = UIImpactFeedbackGenerator(style: .medium)
+        impactFeedbackGenerator?.impactOccurred()
+    }
+    
+    @IBAction func impactHeavyButtonPressed(_ sender: Any) {
+        impactFeedbackGenerator = UIImpactFeedbackGenerator(style: .heavy)
+        impactFeedbackGenerator?.impactOccurred()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        selectionFeedbackGenerator = UISelectionFeedbackGenerator()
+        notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+        
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        impactFeedbackGenerator?.prepare()
+        selectionFeedbackGenerator?.prepare()
+        notificationFeedbackGenerator?.prepare()
     }
 
     override func didReceiveMemoryWarning() {
